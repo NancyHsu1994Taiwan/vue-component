4188dc979f7c2d781e193a6e05eadf8b6311bf58
NancyHsu1994Taiwan <jjuice258tw@gmail.com>
2023-05-05 12:02:12 +0800

    edit base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-component/',
+  base: '/<REPO>/',
+  // https://<USERNAME>.github.io/<REPO>/
+  // https://github.com/NancyHsu1994Taiwan/vue-component
   plugins: [vue()],
   resolve: {
     alias: {
